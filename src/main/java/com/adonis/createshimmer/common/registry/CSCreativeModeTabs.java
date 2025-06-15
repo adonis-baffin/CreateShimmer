671ff000999586ab04d5490b0c10d3a4ec5faf8d
adonis-baffin <3329586162@qq.com>
2025-06-16 00:42:32 +0800

    第四步：添加暮色森林依赖和配置环境修改，去除文件头

--- src/main/java/com/adonis/createshimmer/common/registry/CSCreativeModeTabs.java
+++ src/main/java/com/adonis/createshimmer/common/registry/CSCreativeModeTabs.java
@@ -1,20 +1,3 @@
-/*
- * Copyright (C) 2025  DragonsPlus
- * SPDX-License-Identifier: LGPL-3.0-or-later
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
 
 package com.adonis.createshimmer.common.registry;
 
@@ -54,6 +37,7 @@ public class CSCreativeModeTabs {
 
     private static void buildBaseContents(CreativeModeTab.ItemDisplayParameters parameters, CreativeModeTab.Output output) {
         output.accept(MECHANICAL_GRINDSTONE);
+        output.accept(SHIMMER_REAGENT);
         output.accept(EXPERIENCE_HATCH);
         output.accept(EXPERIENCE_LANTERN);
         output.accept(PRINTER);
--- src/main/java/com/adonis/createshimmer/common/registry/CSItems.java
+++ src/main/java/com/adonis/createshimmer/common/registry/CSItems.java
@@ -1,20 +1,4 @@
-/*
- * Copyright (C) 2025  DragonsPlus
- * SPDX-License-Identifier: LGPL-3.0-or-later
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
+
 
 package com.adonis.createshimmer.common.registry;
 
@@ -73,6 +57,14 @@ public class CSItems {
                     .rarity(Rarity.RARE)
                     .component(DataComponents.ENCHANTMENT_GLINT_OVERRIDE, true))
             .register();
+    public static final ItemEntry<Item> SHIMMER_REAGENT = REGISTRATE
+            .item("shimmer_reagent", Item::new)
+            .lang("Shimmer Reagent")
+            .properties(prop -> prop
+                    .rarity(Rarity.COMMON)
+                    .component(DataComponents.ENCHANTMENT_GLINT_OVERRIDE, true))
+            .register();
+
     public static final DeferredItem<BucketItem> EXPERIENCE_BUCKET = DeferredItem
             .createItem(CSCommon.asResource("experience_bucket"));
 
